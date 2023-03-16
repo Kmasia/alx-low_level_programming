cc9ebe6e826d5707b71df99afec4ce676d5d771a
Kmasia <kanygmasia@gmail.com>
2023-03-16 13:03:41 -0700

    a day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - the main function
+ * main - the function in which most functions run
  *
- * Return:  always 0
+ * Return: at the end of main retrun 0
  */
 
 int main(void)
 {
-              puts("\"Programming is like building a multilingual puzzle");
-              return(0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

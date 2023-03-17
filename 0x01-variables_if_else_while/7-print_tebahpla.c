ad1d433839ac619de47d159e9e4a37ec41381a72
Kmasia <kanygmasia@gmail.com>
2023-03-17 15:10:02 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

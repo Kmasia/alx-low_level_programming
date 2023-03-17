ae1d3bcaef86a0a02d56b7e42221176f2f0dfa1a
Kmasia <kanygmasia@gmail.com>
2023-03-17 15:55:51 -0700

    l

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 
 	for (t = 0 ; t < 10 ; t++)
 	{
-		if (t == 9)
-			putchar(t + '0');
-		else
+		putchar(t + '0');
+		if (i != 9)
 		{
-			putchar(t + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }

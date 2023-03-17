173f08a056bced91132f5feae19156b4208b7606
Kmasia <kanygmasia@gmail.com>
2023-03-17 14:45:10 -0700

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: main returns 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}

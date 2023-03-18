0ae41b26d4a1b7d1bbfc08c8da650d753160947c
Kmasia <kanygmasia@gmail.com>
2023-03-18 07:49:57 -0700

    success finally

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ *
+ * Return: main function returns 1
+ */
+
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19'");
+	return (1);
+}

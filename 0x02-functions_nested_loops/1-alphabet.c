abea43edbb14855262bc8e580b341ea6e35933ad
Kmasia <kanygmasia@gmail.com>
2023-03-21 08:50:38 -0700

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Returns: returns nothing
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n');
+}

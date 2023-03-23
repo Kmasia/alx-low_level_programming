dd221981b5650d4cfadf8987b3d337deebb88f86
Kmasia <kanygmasia@gmail.com>
2023-03-23 10:51:29 -0700

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,9 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
+		if (i == 100)
+			printf("Buzz");
+		else if (i % 5 == 0 && i % 3 == 0)
 			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
